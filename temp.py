b88e3f634292ca75310db13ae081fb2308b3a957
Bekzodjon Nosirov <135831097+bekzodnosirovv@users.noreply.github.com>
2024-09-05 15:30:32 +0500

    Create temp.py
    first commit

---
+++ temp.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+# print(7*8)
+# print("Hello World")
+# print(2**5)
+# print(16/4)
+# print(15//4)
+
+#ism=input("Ismingiz nima ?")
+#print(ism)
+
+#a= 50
+#b= 40.8
+#c= 4579214578965478952321458721456952265863224562336
+#print(c)
+#d, f, h, b = 6, 7, 8, 71
+#print(b)
+
+#mevalar=['olma','olcha','gilos']
+#mevalar.append("behi")
+#mevalar.insert(0, "banan")
+#mevalar.insert(7, "apelsin")
+#print(mevalar)
+#sonlar=[154,5524,4845,26320]
+#print(sonlar)
+
+#aralash=['adsa',441]
+#print(aralash)
+
+#mevalar =["olma","behi","olcha","banan"]
+#del mevalar[1]
+#mevalar.remove("behi")
+#print(mevalar)
+#meva=mevalar.pop(1)
+#print(mevalar)
+#mevalar.remove("olma")
+#print(mevalar)
+
+sonlar=[15,20,12,11,54,23,47,46,34]
+#print(min(sonlar))
+#print(sum(sonlar))
+#print(range(sonlar))
+
+#for son in sonlar:
+ #   print(son)
+    
+#for a in range(0,10):
+ #   print(a)    
+
+#a=51
+#if a==50:
+ #   print("son")
+#else:
+ #   print("son emas")
+
+print("Salom")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
